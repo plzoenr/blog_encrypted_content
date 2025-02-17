c84e7aa1b013090b2cd3bf25489e7e87b04ed000
por <bantoon.bjk@gmail.com>
2025-02-17 13:18:51 +0700

    change up before slug for detect reason

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 
   get  '/import', to: 'blogs#import'
   post '/import', to: 'blogs#create'
-
   get  '/export', to: 'blogs#export'
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   get  '/:slug', to: 'blogs#show', as: :show_blog
 
   resources :blogs, only: [:index, :update], param: :slug
-  get "up" => "rails/health#show", as: :rails_health_check
 end
